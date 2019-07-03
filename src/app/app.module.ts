c84edefc2315043d47a4f5155b33159954079ca1
nimosaid <saidnimo123@gmail.com>
2019-07-03 16:57:00 +0300

    add changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuotesComponent } from './quotes/quotes.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    QuoteFormComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/quotes.ts
@@ -0,0 +1,6 @@
+export class Quotes {
+  public showAuthor :boolean;
+  constructor(public id:number, public name:string, public author:string,public submitted:string) {
+    this.showAuthor =false
+  }
+}
