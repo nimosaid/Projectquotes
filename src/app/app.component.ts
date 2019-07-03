52bfa3d10dd7786c97882c9ade016b4b405c48f6
nimosaid <saidnimo123@gmail.com>
2019-07-03 14:53:58 +0300

    added a readme.md

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Projectquotes } from './quote'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Projectquotes';
+  Projectquotes = [
+    new Quote(1, 'The sun is a star.','A.B Clark',"Z.G"),
+    new Quote(2, 'The moon is beautiful.', 'B.C Smith',"E.I"),
+    new Quote(3, 'The earth is meh.', 'D.E Jacobsons',"T.O"),
+    new Quote(4, 'The galaxy is large.', 'F.G Randos',"P.S"),
+    new Quote(5, 'The universe is expanding.', 'I.J Persons',"Q.T"),
+
+  ];
+constructor(){ }
 }
